a7c5b49db03b246314f4802f2d42f6a6a7f1fa8c
Huseyin-Kaplan <kaplanhuseyin1999@gmail.com>
2023-06-14 07:49:59 +0300

    some changes

--- memeGenerator/src/assets/components/Meme.jsx
+++ memeGenerator/src/assets/components/Meme.jsx
@@ -1,39 +1,83 @@
-import React, { useRef } from "react"
+import React, { useEffect, useRef, useCallback } from "react"
 import html2canvas from "html2canvas"
 import axios from "axios"
 import { Helmet } from "react-helmet"
 import Draggable from "react-draggable"
-//black and dark mode ekle practice için ! 
+import ColorThief from "colorthief"
+
+//black and dark mode ekle practice için !
 
 export default function Meme() {
 	const [texts, setTexts] = React.useState({
 		topText: "",
 		bottomText: "",
 		randomImage: "/memeGenerator/placeHolderImage.svg",
-		fontSize: "25"
+		fontSize: "35",
 	})
 
-
-	const [click, setClick] = React.useState(false)
+	// const [click, setClick] = React.useState(false)
 	const [memes, setMemes] = React.useState([])
 	const [fontList, setFontList] = React.useState([])
 	const [selectedFont, setSelectedFont] = React.useState("")
-	const [color, setColor ]= React.useState('#ffffff')
+	const [color, setColor] = React.useState("#ffffff")
 	const [extraInput, setExtraInput] = React.useState([])
-	const addInput = () =>  {
-		setExtraInput([
-			...extraInput, {id: `input-${Date.now()}`, value: ""}
-		])
+	const [backgroundColor, setBackgroundColor] = React.useState("")
+	const [boxShadowColor, setBoxShadowColor] = React.useState("#fff")
+
+	const addInput = () => {
+		setExtraInput([...extraInput, { id: `input-${Date.now()}`, value: "" }])
 	}
 
 	const handleMoreInputsChange = (event) => {
 		event.preventDefault()
-		const {name, value} = event.target
-		setExtraInput(extraInput.map(prevInput => (
-			prevInput.id === name ? {...prevInput, value: value} : prevInput
-		))) 
-		 
+		const { name, value, key } = event.target
+		setExtraInput(extraInput.map((prevInput) => (prevInput.id === name ? { ...prevInput, value: value } : prevInput)))
 	}
+
+	//getting memes's color codes
+
+	const imageRef = useRef(null)
+
+	const getColorFromImage = useCallback(() => {
+		const colorThief = new ColorThief()
+		const imageElement = imageRef.current
+
+		if (imageElement.complete) {
+			const color = colorThief.getColor(imageElement)
+
+			//inverting color
+			const invertColor = color.map((value) => 255 - value)
+
+			//converting to string
+			const invertedColorStr = `rgb(${invertColor.join(",")})`
+
+			//hex color
+			//calculating Hex value
+			const toHex = (c) => {
+				let hex = c.toString(16)
+				return hex.length == 1 ? "0" + hex : hex
+			}
+
+			const rgbToHex = (r, g, b) => {
+				return "#" + toHex(r) + toHex(g) + toHex(b)
+			}
+
+			setBoxShadowColor(rgbToHex(color[0], color[1], color[2]))
+			setBackgroundColor(invertedColorStr)
+		}
+	}, [texts.randomImage])
+
+	useEffect(() => {
+		if (texts.randomImage) {
+			getColorFromImage()
+		}
+	}, [texts.randomImage, getColorFromImage])
+
+	//choosing body element with effect
+
+	useEffect(() => {
+		document.body.style.backgroundColor = backgroundColor
+	}, [backgroundColor])
 
 	//google font select
 	//font size feature eklenecek, input arttırma feature ı eklenecek . Kullanıcı istediği kadar input girebilecek .
@@ -42,15 +86,14 @@ export default function Meme() {
 		const getFonts = async () => {
 			const res = await axios.get("https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyA3a4MSy7tF9whne5eH2rKjM-y0slQ-mZA")
 			setFontList(res.data.items)
-			
 		}
 		getFonts()
 	}, [])
 	// saving images
-	const imageRef = useRef(null)
+	const screenshotRef = useRef(null)
 
 	const saveImage = () => {
-		const node = imageRef.current
+		const node = screenshotRef.current
 		if (node) {
 			html2canvas(node, { allowTaint: true, useCORS: true, logging: true }).then((canvas) => {
 				const imgData = canvas.toDataURL("image/png")
@@ -76,9 +119,9 @@ export default function Meme() {
 		// 	setClick(true)
 		// 	return
 		// }
-		setClick(false)
+		// setClick(false)
 		const randomNumber = Math.floor(Math.random() * memes.length)
-		const url = memes[randomNumber].url
+		const url = memes[randomNumber] ? memes[randomNumber].url : "/memeGenerator/placeHolderImage.svg"
 
 		setTexts({
 			topText: "",
@@ -96,8 +139,6 @@ export default function Meme() {
 			// computed property name
 			[name]: value,
 		}))
-
-		
 	}
 
 	const handleText = (e) => {
@@ -105,9 +146,9 @@ export default function Meme() {
 	}
 
 	const style = {
-			fontFamily: selectedFont,
-			color: color,
-			fontSize: texts.fontSize+"px"
+		fontFamily: selectedFont,
+		color: backgroundColor,
+		fontSize: texts.fontSize + "px",
 	}
 
 	return (
@@ -118,6 +159,7 @@ export default function Meme() {
 					value={texts.topText}
 					onChange={handleChange}
 					placeholder="Enter Top Text"
+					style={{ fontFamily: selectedFont, boxShadow: boxShadowColor ? `-1px 0px 3px 2px ${boxShadowColor}` : "none" }}
 				/>
 
 				<input
@@ -125,61 +167,82 @@ export default function Meme() {
 					value={texts.bottomText}
 					onChange={handleChange}
 					placeholder="Enter Bottom Text"
+					style={{ fontFamily: selectedFont, boxShadow: boxShadowColor ? `-1px 0px 3px 2px ${boxShadowColor}` : "none" }}
 				/>
 
-				{extraInput.map(inputs => (
+				{extraInput.map((inputs) => (
 					<input
-						
 						key={inputs.id}
 						name={inputs.id}
 						value={inputs.value}
 						onChange={handleMoreInputsChange}
 						placeholder="Enter Text"
+						style={{ fontFamily: selectedFont }}
 					/>
 				))}
 
-				<button className="inputs--addButton" onClick={addInput} ><small>+</small></button>
-
+				<button
+					title="Add a new input area"
+					className="inputs--addButton"
+					onClick={addInput}
+					style={{ boxShadow: boxShadowColor ? `0px 0px 0px 2px ${boxShadowColor}` : "none" }}
+				>
+					<small>+</small>
+				</button>
 			</div>
 
-			<button onClick={gettingImages}>Get a new meme image</button>
+			<button
+				style={{ boxShadow: boxShadowColor ? `-1px 0px 0px 2px ${boxShadowColor}` : "none" }}
+				onClick={gettingImages}
+			>
+				Get a new meme image
+			</button>
 			{/* {click && <p className="errorMessage">Please Fill An Input</p>} */}
-			
-			
+
 			<div
 				className="main--content__image"
-				ref={imageRef}
+				ref={screenshotRef}
 			>
 				<img
+					style={{ boxShadow: boxShadowColor ? `-1px 0px 3px 2px ${boxShadowColor}` : "none" }}
 					className="hover-overlay"
+					ref={imageRef}
 					src={texts.randomImage}
 					alt="meme image"
+					onLoad={getColorFromImage}
+					crossOrigin="anonymous"
 				/>
 				<Draggable bounds="parent">
-				<h3
-					style={style}
-					className="main--content__image--topText"
-				>
-					{texts.topText}
-				</h3>
+					<h3
+						style={style}
+						className="main--content__image--topText"
+					>
+						{texts.topText}
+					</h3>
 				</Draggable>
 				<Draggable bounds="parent">
-				<h3 
-					style={style}
-				className="main--content__image--bottomText"> {texts.bottomText} </h3>
+					<h3
+						style={style}
+						className="main--content__image--bottomText"
+					>
+						{" "}
+						{texts.bottomText}{" "}
+					</h3>
 				</Draggable>
 
-				{extraInput.map(newInputs => {
-
-					return(
+				{extraInput.map((newInputs) => {
+					return (
 						<Draggable bounds="parent">
-						<h3 className="main--content__image--bottomText" style={style}> {newInputs.value} </h3>
-						
-					</Draggable>
+							<h3
+								className="main--content__image--bottomText"
+								style={style}
+							>
+								{" "}
+								{newInputs.value}{" "}
+							</h3>
+						</Draggable>
 					)
-				
 				})}
-
 			</div>
 
 			<Helmet>
@@ -192,40 +255,43 @@ export default function Meme() {
 			</Helmet>
 			<div className="new--features">
 				<div className="new--features__selectFont">
-				<select onChange={handleText}> 
-					
-					{fontList.map(font => (
-						<option key={font.family} value={font.family} >
-						{font.family}
-					</option>
-					))}
-
-				</select>
-
-			
+					<h4 style={{ color: boxShadowColor }}> Font Family & Color & Size : </h4>
+					<select
+						style={{ fontFamily: selectedFont, boxShadow: boxShadowColor ? `-1px 0px 17px 3px ${boxShadowColor}` : "none" }}
+						onChange={handleText}
+						title="change meme  font family"
+					>
+						{fontList.map((font) => (
+							<option
+								key={font.family}
+								value={font.family}
+							>
+								{font.family}
+							</option>
+						))}
+					</select>
+					<input
+						type="color"
+						onChange={(e) => setColor(e.target.value)}
+						title="change meme font's color"
+					/>
+					<input
+						name="fontSize"
+						type="number"
+						value={texts.fontSize}
+						onChange={handleChange}
+						placeholder="Enter Font Size Number"
+						title="change meme font size"
+					/>
 				</div>
-				<div className="new--features__colorPicker">
-						<input
-							type="color"
-							value={color}
-							onChange={e => setColor(e.target.value)}
-						/>
-				</div>
-
-				<div className="new--features__fontSize">
-						<input 
-							name="fontSize"
-							type="number"
-							value={texts.fontSize}
-							onChange={handleChange}
-							placeholder="Enter Font Size Number"
-							
-						/>
-				</div>
-
 			</div>
 
-			<button onClick={saveImage}>Save Your Meme</button>
+			<button
+				style={{ boxShadow: boxShadowColor ? `-1px 0px 0px 2px ${boxShadowColor}` : "none" }}
+				onClick={saveImage}
+			>
+				Save Your Meme
+			</button>
 		</main>
 	)
 }
