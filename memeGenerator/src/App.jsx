0f21ee38bebf99a7f903b2bcec9546a5aa4b77e7
Huseyin-Kaplan <kaplanhuseyin1999@gmail.com>
2023-06-18 13:18:30 +0300

    footer update

--- memeGenerator/src/App.jsx
+++ memeGenerator/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "./assets/components/Header.jsx"
 import Meme from "./assets/components/Meme.jsx"
+import Footer from "./assets/components/Footer.jsx"
 function App() {
 	return (
 		<>
 			<Header />
 			<Meme />
+			<Footer />
 		</>
 	)
 }
---
+++ memeGenerator/src/assets/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Footer() {
+	return (
+		<footer>
+			<p>Hüseyin Kaplan &#169; 2023</p>
+			<a
+				href="https://github.com/huseyinkaplan00/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<img src="./github.svg" /> GitHub ✨
+			</a>
+		</footer>
+	)
+}
