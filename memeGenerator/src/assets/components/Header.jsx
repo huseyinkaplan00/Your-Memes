e905906f819f5c34924096fe84ee37ab93d07663
Huseyin Kaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-06-27 01:31:56 +0300

    added sticky header feature

--- memeGenerator/src/assets/components/Header.jsx
+++ memeGenerator/src/assets/components/Header.jsx
@@ -1,17 +1,37 @@
 import React from "react"
 
 export default function Header() {
-	return (
-		<header>
-			<a href="index.html">
-				<img
-					src="/memeGenerator/logo.png"
-					alt="header icon troll face"
-					className="header__logo"
-				/>
-				<h1> Meme Generator</h1>
-			</a>
-			<p>Generate your favorite memes</p>
-		</header>
-	)
+  // scroll position control
+  const [isSmallHeader, setIsSmallHeader] = React.useState(false)
+
+  React.useEffect(() => {
+
+    const handleScroll = () => {
+    const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop
+
+    if(currentScrollPosition > 50) {
+      setIsSmallHeader(true)
+    }
+    else if (currentScrollPosition < 20) { 
+      setIsSmallHeader(false)
+    }
+  }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  },[])
+
+
+  return (
+    <header className={`header ${ isSmallHeader ? 'smallHeader' : "largeHeader"}`}>
+      <a href="index.html">
+        <img
+          src="/memeGenerator/logo.png"
+          alt="header icon troll face"
+          className="header__logo"
+        />
+        <h1> Meme Generator</h1>
+      </a>
+      <p>Generate your favorite memes</p>
+    </header>
+  )
 }
