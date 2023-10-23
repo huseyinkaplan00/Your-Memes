9f60721f60fc075ea48775a59d1a8651494683b7
Huseyin Kaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-10-23 12:20:46 +0300

    vercel

--- memeGenerator/vite.config.js
+++ memeGenerator/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
-	base: "/memeGenerator/",
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks: undefined,
-			},
-		},
-		publicPath: "",
-	},
-})
+  plugins: [react()],
+  base: "/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    publicPath: "",
+  },
+});
