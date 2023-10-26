4c1b0e7f734f212a8da88636246ee8334e3b1f23
Huseyin Kaplan <104190325+huseyinkaplan00@users.noreply.github.com>
2023-10-26 13:22:55 +0300

    some vite changes

--- memeGenerator/vite.config.js
+++ memeGenerator/vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/memeGenerator/",
+  base: "/",
   build: {
     rollupOptions: {
       output: {
         manualChunks: undefined,
       },
     },
-    publicPath: "",
+    publicPath: "/",
   },
 });
